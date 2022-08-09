e4c753d1fea69bf963c14692a7bb9533108c853c
yamilmasc1 <yamilmascali1@gmail.com>
2022-08-09 16:20:02 -0300

    corregido el desafio

--- Desafio Arrays Clase 6/miarchivo.js
+++ Desafio Arrays Clase 6/miarchivo.js
@@ -19,50 +19,25 @@ arrayProductos.push(new Producto("Campera","2500"));
 arrayProductos.push(new Producto("Zapatillas", "6000"));
 arrayProductos.push(new Producto("Pantalon", "5500"));
 console.log(arrayProductos);
-
+//no se si esta bien usado esto.
 for (const producto of arrayProductos) {
-    console.log("El precio del producto en 12 cuotas es "+ sumaAhora12);
-    console.log("El precio del producto en 18 cuotas es "+ sumaAhora18);
-    console.log("El precio del producto abonando en efectivo es "+ restaEfectivo);
+    producto.sumaAhora12();
+    console.log("El precio de los productos en 12 cuotas es de "+ producto.precio);
+    alert("El precio de los productos en 12 cuotas es de "+ producto.precio);
 };
+for (const producto of arrayProductos) {
+    producto.sumaAhora18();
+    console.log("El precio de los productos en 18 cuotas es de "+ producto.precio);
+    alert("El precio de los productos en 18 cuotas es de "+ producto.precio)
+}
+for (const producto of arrayProductos) {
+    producto.restaEfectivo();
+    console.log("El precio de los productos abonando en efectivo es de "+ producto.precio);
+    alert("El precio de los productos abonando en efectivo es de "+ producto.precio);
+}
 
 
 
 
 
-//Sumar el interes de ahora 12
 
-/* function calcularInteres (precioCosto) {
-    return (precioCosto * 1.42);
-} */  
-//Cambio a funcion flecha
-/*const calcularInteres= ( precio ) => precio * 1.42  ;
-let interes12 = calcularInteres(precioCosto);
-alert ("El precio del producto en 12 cuotas fijas es: "+interes12);
-console.log("El precio total del producto en 12 cuotas es: "+interes12); */
-
-
-
-//Sumar el interes de ahora 18
-
-/* function calcularInteres2 (precioCosto) {
-    return (precioCosto * 1.49);
-} */
-//cambio a funcion flecha
-/*const calcularInteres2= ( precio ) => precio * 1.49  ;
-let interes18 = calcularInteres2(precioCosto);
-alert ("El precio del producto en 18 cuotas fijas es de: "+interes18);
-console.log("El precio total del producto en 18 cuotas es: "+interes18); */
-
-
-
-//Descuento pago en efectivo o transferencia
-
-/*function calcularPrecioEfectivo (precioCosto){
-    return (precioCosto * 0.90);
-} */
-//cambio a funcion flecha
-/*const calcularPrecioEfectivo= ( precio ) => precio * 0.90  ;
-var precioContado = calcularPrecioEfectivo(precioCosto);
-alert("El precio del producto abonando de contado/transferencia es: "+precioContado);
-console.log("El precio de contado/transferencia es: "+precioContado); */
