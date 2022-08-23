8fdfc27f73a815d6fffd0bd09d560c3268dbbcf5
yamilmasc1 <yamilmascali1@gmail.com>
2022-08-23 20:50:27 -0300

    agregando funciones JS

--- Copia-Wordle/wordle.js
+++ Copia-Wordle/wordle.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     crearCuadrados();
 
-function crearCuadrados() {
+
+
+  let nombre = prompt("Ingrese su nombre Aqui");
+  if (nombre == "") {
+    alert("No ingresaste ningun nombre.");
+  }
+  else {
+    alert("Bienvenido "+nombre+" disfrute del Wordle!.");
+  }
+
+  function crearCuadrados() {
     const tablero = document.getElementById("tabla");
 
     for (let index = 0; index < 30; index++) {
-      let cuadrado = document.createElement("div");
-      cuadrado.classList.add("cuadrado");
-      cuadrado.classList.add("animate__animated");
+      const cuadrado = document.createElement("div");
+      cuadrado.classList.add("cuadrado", "animate__animated");
       cuadrado.setAttribute("id", index + 1);
       tablero.appendChild(cuadrado);
     }
